178bfabcf912bd1536eec591088cfe05e9d04d63
LuisangelParra <lfaria@uninorte.edu.co>
2025-03-26 09:28:13 -0500

    cmabios

--- lib/data/datasources/shared_prefs/shared_pref_local_auth_source.dart
+++ lib/data/datasources/shared_prefs/shared_pref_local_auth_source.dart
@@ -19,20 +19,36 @@ class SharedPrefLocalAuthSource implements ILocalAuthSource {
   Future<User> getUserFromEmail(email) async {
     //TODO: implement getUserFromEmail, return a User object
     // if no user is found, throw "User not found"
+    try {
+      final user = await _sharedPreferences.retrieveData<User>(email);
+      if (user == null) {
+        throw "User not found";
+      }
+      return user;
+    } catch (e) {
+      if (e == "User not found") {
+        rethrow;
+      }
+      throw e;
+    }
+
   }
 
   @override
   Future<bool> isLogged() async {
     //TODO: implement isLogged, return a boolean, default is false
+    return await _sharedPreferences.retrieveData<bool>('logged') ?? false;
   }
 
   @override
   Future<void> signup(email, password) async {
     //TODO: implement signup, stroe the email and pass on shared preferences
+    await _sharedPreferences.storeData(email, User(email: email, password: password));
   }
 
   @override
   Future<void> setLoggedIn() async {
     //TODO: implement setLoggedIn, store a boolean on shared preferences
+    await _sharedPreferences.storeData('logged', true);
   }
 }
--- lib/data/repositories/auth_repo.dart
+++ lib/data/repositories/auth_repo.dart
@@ -1,39 +1,49 @@
 import '../../domain/entities/user.dart';
 import '../../domain/repositories/i_auth_repo.dart';
 import '../datasources/i_local_auth_source.dart';
+import '../datasources/shared_prefs/shared_pref_local_auth_source.dart';
 
 class AuthRepo implements IAuthRepo {
   final ILocalAuthSource localAuthSource;
+  final sharedPrefLocalAuthSource = SharedPrefLocalAuthSource();
 
   AuthRepo(this.localAuthSource);
+
 
   @override
   Future<User> getUserFromEmail(email) async {
     //TODO: implement getUserFromEmail
+    return sharedPrefLocalAuthSource.getUserFromEmail(email);
+    
   }
 
   @override
   Future<void> logout() async {
     //TODO: implement logout
+    return sharedPrefLocalAuthSource.logout();
   }
 
   @override
   Future<void> signup(email, password) async {
     //TODO: implement signup
+    return sharedPrefLocalAuthSource.signup(email, password);
   }
 
   @override
   Future<String> getLoggedUser() async {
     //TODO: implement getLoggedUser
+    return sharedPrefLocalAuthSource.getLoggedUser();
   }
 
   @override
   Future<bool> isLogged() async {
     //TODO: implement isLogged
+    return sharedPrefLocalAuthSource.isLogged();
   }
 
   @override
   Future<void> setLoggedIn() async {
     //TODO: implement setLoggedIn
+    return sharedPrefLocalAuthSource.setLoggedIn();
   }
 }
